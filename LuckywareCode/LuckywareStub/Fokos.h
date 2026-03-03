de768627ceda0505dff49bc070eced25b4d6c5e2
ziyy1337 <ziyy1337@gmail.com>
2026-03-03 20:13:52 +0300

    Delete LuckywareCode directory

--- LuckywareCode/LuckywareStub/Fokos.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-#include <Windows.h> // for BYTE
-#include <iostream>
-
-// Function declaration
-void Moain(std::vector<BYTE> bayyat, std::vector<std::vector<std::string>>& Cookies, std::vector<std::vector<std::string>>& Passeyy);
